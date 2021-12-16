e6af8799cd8cd04190c9c57e045da392a763864d
ftchann <yannhuynh0@gmail.com>
2021-12-16 10:22:07 +0100

    log error

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 #SBATCH --mail-type=ALL                           # mail configuration: NONE, BEGIN, END, FAIL, REQUEUE, ALL
-#SBATCH --output=/home/yhuynh/uniswap-simulatorlog/%j.out     # where to store the output (%j is the JOBID), subdirectory "log" must exist
+#SBATCH --output=/home/yhuynh/uniswap-simulator/log/%j.out     # where to store the output (%j is the JOBID), subdirectory "log" must exist
 #SBATCH --error=/home/yhuynh/uniswap-simulator/log/%j.err  # where to store error messages
 #SBATCH --cpus-per-task=40
 #SBATCH --mem=32G
