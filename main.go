c5eaaea8a7f2b936a96879a65902a13ecbb5d56e
ftchann <yannhuynh0@gmail.com>
2022-01-02 19:14:21 +0100

    update start.sh

--- main.go
+++ main.go
@@ -59,7 +59,7 @@ func main() {
 	}
 	amountHistorySnapshots := 100
 	mulUpperBound := IntPow(2, 16)
-	results := make([]result.RunResult, len(durations)*(mulUpperBound-1))
+	results := make([]result.RunResult, len(durations)*(mulUpperBound-1)) // -1 because we skip zero
 
 	for durIndex, duration := range durations {
 		// Additional for loop to reduce memory usage
@@ -74,7 +74,7 @@ func main() {
 				}
 				// Prices Snapshot for moving average
 				// Interval in which the snapshots are taken
-				i := durIndex*(mulUpperBound-1) + (mul - 1)
+				i := durIndex*(mulUpperBound-1) + (mul - 1) // -1 because we skip zero
 				priceHistoryInterval := duration / amountHistorySnapshots
 				strategy := strat.NewVolatilitySizedIntervalStrategy(startAmount0, startAmount1, pool, amountHistorySnapshots, mul)
 				execution := executor.CreateExecution(strategy, startTime, updateInterval, snapshotInterval, priceHistoryInterval, transactions)
--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 sbatch run.sh 2 2_hours.json
 sbatch run.sh 6 6_hours.json
 sbatch run.sh 24 1_day.json
-sbatch run.sh 168 7_days.json
-sbatch run.sh 720 30_days.json
+sbatch run.sh 168 7_day.json
+sbatch run.sh 720 30_day.json
