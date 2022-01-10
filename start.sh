b7225be8333abdc5f510de2a50b2b69fcfd3af04
ftchann <yannhuynh0@gmail.com>
2022-01-11 00:48:17 +0100

    volatility sized interval

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 go build main.go
-sbatch run.sh 2 2_hours.json
-sbatch run.sh 6 6_hours.json
-sbatch run.sh 24 1_day.json
-sbatch run.sh 168 7_day.json
-sbatch run.sh 720 30_day.json
+sbatch run.sh 2 2_hours1.json
+sbatch run.sh 6 6_hours1.json
+sbatch run.sh 24 1_day1.json
+sbatch run.sh 168 7_day1.json
+sbatch run.sh 720 30_day1.json
